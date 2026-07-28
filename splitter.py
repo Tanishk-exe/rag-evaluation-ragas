a902e11830d756f5d95a17479acc8090de9368d5
Tanishk <tanishkk501@gmail.com>
2026-07-28 14:02:13 +0530

    Delete splitter.py

--- splitter.py
+++
@@ -1,7 +0,0 @@
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-
-
-def split(doc, chunk_size,chunk_overlap):
-    sp=RecursiveCharacterTextSplitter(chunk_size=chunk_size,chunk_overlap=chunk_overlap,separators=["\n\n","\n",". "," ",""])
-    splitted=sp.split_documents(doc)
-    return splitted
